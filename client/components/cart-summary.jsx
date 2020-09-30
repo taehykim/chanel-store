22c33ea7dd4cbda98a3356492bf24a24e561260c
Tia <tiarekim@gmail.com>
2020-09-30 02:13:46 +0000

    worked on the frontend of placing an order

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -6,10 +6,15 @@ class CartSummary extends React.Component {
     super(props);
     this.onBackToCatalogClick = this.onBackToCatalogClick.bind(this);
     this.getTotalPrice = this.getTotalPrice.bind(this);
+    this.onCheckoutClick = this.onCheckoutClick.bind(this);
   }
 
   onBackToCatalogClick() {
     this.props.setView('catalog', {});
+  }
+
+  onCheckoutClick() {
+    this.props.setView('checkout', {});
   }
 
   getTotalPrice() {
@@ -22,17 +27,37 @@ class CartSummary extends React.Component {
 
   render() {
     return (
-      <div className="d-flex flex-column p-5 mx-auto" style={{ width: '90%' }}>
-        <div className="back my-3" onClick={this.onBackToCatalogClick}>
-          &lt; Back to catalog
+      <div className="row bg-light vh-100">
+        <div
+          className="d-flex flex-column p-5 mx-auto"
+          style={{ width: '90%' }}
+        >
+          <div className="back my-3" onClick={this.onBackToCatalogClick}>
+            &lt; Back to catalog
+          </div>
+          <div className="h1">My Cart</div>
+          <div>
+            {this.props.cartItems.map(item => (
+              <CardSummaryItem item={item} key={item.cartItemId} />
+            ))}
+          </div>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="h3">Item Total: ${this.getTotalPrice()}</div>
+            {this.props.cartItems.length !== 0 ? (
+              <div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={this.onCheckoutClick}
+                >
+                  Check Out
+                </button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
         </div>
-        <div className="h1">My Cart</div>
-        <div>
-          {this.props.cartItems.map(item => (
-            <CardSummaryItem item={item} key={item.cartItemId} />
-          ))}
-        </div>
-        <div className="h2">Item Total: ${this.getTotalPrice()}</div>
       </div>
     );
   }
---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+
+class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', creditCard: '', shippingAddress: '' };
+    this.getTotalPrice = this.getTotalPrice.bind(this);
+    this.onBackClick = this.onBackClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleCreditCardChange = this.handleCreditCardChange.bind(this);
+    this.handleShippingAddressChange = this.handleShippingAddressChange.bind(
+      this
+    );
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getTotalPrice() {
+    let total = 0;
+    for (let i = 0; i < this.props.orderItems.length; i++) {
+      total += this.props.orderItems[i].price;
+    }
+    return total / 100;
+  }
+
+  onBackClick() {
+    this.props.setView('catalog', {});
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleCreditCardChange(event) {
+    this.setState({ creditCard: event.target.value });
+  }
+
+  handleShippingAddressChange(event) {
+    this.setState({ shippingAddress: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.placeOrder(this.state);
+    this.setState({ name: '', creditCard: '', shippingAddress: '' });
+  }
+
+  render() {
+    return (
+      <div className="row bg-light vh-100">
+        <div
+          className="d-flex flex-column p-5 mx-auto"
+          style={{ width: '90%' }}
+        >
+          <div className="h1">My Cart</div>
+          <div className="h4">Order Total: ${this.getTotalPrice()}</div>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <label>Name</label>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.name}
+                onChange={this.handleNameChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Credit Card</label>
+              <input
+                type="text"
+                className="form-control"
+                value={this.state.creditCard}
+                onChange={this.handleCreditCardChange}
+              />
+            </div>
+            <div className="form-group">
+              <label>Shipping Address</label>
+              <textarea
+                className="form-control"
+                rows="4"
+                value={this.state.shippingAddress}
+                onChange={this.handleShippingAddressChange}
+              ></textarea>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div className="back" onClick={this.onBackClick}>
+                &lt; Continue Shopping
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Place Order
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CheckoutForm;
